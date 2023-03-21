2ddd72b42d9f8f2fb53cb212b3417c30cd13a186
Aleksei Anisimov <suxelshock@gmail.com>
2023-03-22 00:22:34 +0300

    Составные части URI

--- app/src/main/java/ru/yandex/practicum/sprint11/MainActivity.kt
+++ app/src/main/java/ru/yandex/practicum/sprint11/MainActivity.kt
@@ -1,11 +1,28 @@
 package ru.yandex.practicum.sprint11
 
+import android.net.Uri
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
+    companion object {
+        const val TAG = "SRPINT_11"
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val uri = Uri.parse("https://myserver.com:5051/api/v1/path?text=android&take=1#last")
+
+        Log.d(TAG, "uri.scheme ${uri.scheme}")
+        Log.d(TAG, "uri.host ${uri.host}")
+        Log.d(TAG, "uri.authority ${uri.authority}")
+        Log.d(TAG, "uri.pathSegments ${uri.pathSegments}")
+        Log.d(TAG, "uri.lastPathSegment ${uri.lastPathSegment}")
+        Log.d(TAG, "uri.queryParameterNames ${uri.queryParameterNames}")
+        Log.d(TAG, "uri.fragment ${uri.fragment}")
     }
 }
