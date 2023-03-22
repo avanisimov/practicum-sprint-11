d6b084ae4ffa30fd67b6a36bff1fd38e9f14882e
Aleksei Anisimov <suxelshock@gmail.com>
2023-03-22 20:27:11 +0300

    Парсинг даты

--- app/src/main/java/ru/yandex/practicum/sprint11/MainActivity.kt
+++ app/src/main/java/ru/yandex/practicum/sprint11/MainActivity.kt
@@ -3,12 +3,16 @@ package ru.yandex.practicum.sprint11
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import com.google.gson.*
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.GET
+import java.lang.reflect.Type
+import java.text.SimpleDateFormat
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -22,7 +26,13 @@ class MainActivity : AppCompatActivity() {
 
         val retrofit = Retrofit.Builder()
             .baseUrl("https://raw.githubusercontent.com/avanisimov/practicum-sprint-11/")
-            .addConverterFactory(GsonConverterFactory.create())
+            .addConverterFactory(
+                GsonConverterFactory.create(
+                    GsonBuilder()
+                        .registerTypeAdapter(Date::class.java, CustomDateTypeAdapter())
+                        .create()
+                )
+            )
             .build()
         val serverApi = retrofit.create(Sprint11ServerApi::class.java)
 
@@ -51,4 +61,44 @@ interface Sprint11ServerApi {
 
 data class NewsResponse(
     val result: String,
+    val data: NewsData,
 )
+
+data class NewsData(
+    val title: String,
+    val items: List<NewsItem>
+)
+
+data class NewsItem(
+    val id: Int,
+    val title: String,
+    val type: String,
+    val created: Date,
+)
+
+
+class CustomDateTypeAdapter : JsonDeserializer<Date>, JsonSerializer<Date> {
+    val sdf = SimpleDateFormat("yyyy-MM-dd'T'hh:mm:ss:SSS")
+    override fun deserialize(
+        json: JsonElement,
+        typeOfT: Type,
+        context: JsonDeserializationContext
+    ): Date {
+        val dateString = json.asString
+        // 2023-01-01T11:12:13:123
+        // https://ru.wikipedia.org/wiki/ISO_8601
+
+        val date = sdf.parse(dateString)
+        return date
+    }
+
+    override fun serialize(
+        src: Date,
+        typeOfSrc: Type,
+        context: JsonSerializationContext
+    ): JsonElement {
+        val string = sdf.format(src)
+        return JsonPrimitive(string)
+    }
+
+}
